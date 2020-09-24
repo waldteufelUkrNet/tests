8f632ed5cb9e11cd2eeeca7256a7738a3af0609d
unknown <waldteufel@ukr.net>
2020-09-24 18:38:04 +0300

    qqq

--- app/assets/js-expanded/index.js
+++ app/assets/js-expanded/index.js
@@ -1,59 +1,10 @@
-// let url = 'http://jsonplaceholder.typicode.com/users';
+let controller = new AbortController(),
+    signal     = controller.signal;
 
-// ( async () => {
-//   let response = await fetch(url);
-//   if (response.ok && response.status == 200) {
-//     // let data = await response.json();
-//     // console.log("data", data);
-//     let reader = response.body.getReader();
-//     while (true) {
-//       const {done, value} = await reader.read();
-//       if (done) {
-//         break
-//       }
-//       console.log(`Отримано ${value.length} байт`);
-//     }
-//   }
-// } )();
+console.log("controller", controller);
 
-(async()=>{
-  // Шаг 1: начинаем загрузку fetch, получаем поток для чтения
-  let response = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits?per_page=100');
+signal.addEventListener('abort', () => console.log('скасовано!'));
 
-  const reader = response.body.getReader();
+controller.abort();
 
-  // Шаг 2: получаем длину содержимого ответа
-  const contentLength = +response.headers.get('Content-Length');
-
-  // Шаг 3: считываем данные:
-  let receivedLength = 0; // количество байт, полученных на данный момент
-  let chunks = []; // массив полученных двоичных фрагментов (составляющих тело ответа)
-  while(true) {
-    const {done, value} = await reader.read();
-
-    if (done) {
-      break;
-    }
-
-    chunks.push(value);
-    receivedLength += value.length;
-
-    console.log(`Получено ${receivedLength} из ${contentLength}`)
-  }
-
-  // Шаг 4: соединим фрагменты в общий типизированный массив Uint8Array
-  let chunksAll = new Uint8Array(receivedLength); // (4.1)
-  let position = 0;
-  for(let chunk of chunks) {
-    chunksAll.set(chunk, position); // (4.2)
-    position += chunk.length;
-  }
-
-  // Шаг 5: декодируем Uint8Array обратно в строку
-  let result = new TextDecoder("utf-8").decode(chunksAll);
-
-  // Готово!
-  let commits = JSON.parse(result);
-  console.log("commits", commits);
-  console.log(commits[0].author.login);
-})();
+console.log(signal.aborted);
