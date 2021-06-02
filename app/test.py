68171c526b00057a5a8f5787797fc7c3ecf6ad13
waldteufelUkrNet <waldteufel@ukr.net>
2021-06-03 00:21:16 +0300

    q

--- app/test.py
+++ app/test.py
@@ -1,32 +1,61 @@
-def file_search(folder, filename): # list, str
-  result = ''
-  foldername = folder[0]
-  result = result + foldername + '/'
+def file_search(folder, fileName): # list, str
+  folderName = ''
+  adressArr  = []
 
-  
-  print result
-  print foldername
+  def iterateDir(arr,pos = 0):
+    arrLength = len( arr )
 
-  for item in folder[1:]:
-    # print 'item: ' + str(item)
-    if isinstance(item, list):
-      # sublist iteration
+    if arrLength == 1: # folder is empty and only has a name ( arr['some name'] )
+      #
       a = 1
-    else:
-      if item == filename:
-        result = result + filename
-        return result
+    else:              # folder is`t empty, has a name and at less 1 item ( arr['some name', item 1, item 2, ... item n] )
+      folderName = arr[0]
+      adressArr.append(folderName)
 
-  return False
+      for item in arr[1:]:
+        if isinstance(item,str): # file name
+          if item == fileName:
+            return formAdressString(item)
+        else:                    # new subfolder
+          #
+          a = 1
+
+  def formAdressString(name):
+    result = adressArr[0]
+    for item in adressArr[1:]:
+      result = result + '/' + item
+    result = result + '/' + name
+    return result
+
+  iterateDir(folder)
 
 
-print '===== start ====='
-print 'right result: C:/ideas.txt'
-print 'my result   : ' + str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt'))
-print '-----------------'
-print 'right result: False'
-print 'my result   : ' + str(file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me'))
-print '-----------------'
-print 'right result: /home/user2/desktop/new folder/hereiam.py'
-print 'my result   : ' + str(file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py'))
-print '=====  end  ====='
+
+
+  # for item in folder[1:]:
+  #   # print 'item: ' + str(item)
+  #   if isinstance(item, list):
+  #     # sublist iteration
+  #     a = 1
+  #   else:
+  #     if item == fileName:
+  #       result = result + fileName
+  #       return result
+
+  # return False
+
+print str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt') )
+# file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me')
+# file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py')
+
+
+# print '===== start ====='
+# print 'right result: C:/ideas.txt'
+# print 'my result   : ' + str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt'))
+# print '-----------------'
+# print 'right result: False'
+# print 'my result   : ' + str(file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me'))
+# print '-----------------'
+# print 'right result: /home/user2/desktop/new folder/hereiam.py'
+# print 'my result   : ' + str(file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py'))
+# print '=====  end  ====='
