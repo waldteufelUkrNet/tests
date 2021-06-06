63b1006296bd97457824d639382a00d1312b8cd3
waldteufelUkrNet <waldteufel@ukr.net>
2021-06-07 00:05:39 +0300

    1

--- app/test.py
+++ app/test.py
@@ -1,24 +1,27 @@
 def file_search(folder, fileName): # list, str
   folderName = ''
+  result = ''
   adressArr  = []
 
-  def iterateDir(arr,pos = 0):
+  def iterateDir(arr, pos = 0):
     arrLength = len( arr )
 
     if arrLength == 1: # folder is empty and only has a name ( arr['some name'] )
-      #
-      a = 1
-    else:              # folder is`t empty, has a name and at less 1 item ( arr['some name', item 1, item 2, ... item n] )
+      return False
+    else:              # folder is't empty, has a name and at less 1 item ( arr['some name', item 1, item 2, ... item n] )
       folderName = arr[0]
       adressArr.append(folderName)
+
+      subfolderResult = ''
 
       for item in arr[1:]:
         if isinstance(item,str): # file name
           if item == fileName:
             return formAdressString(item)
         else:                    # new subfolder
-          #
-          a = 1
+          subfolderResult = iterateDir(item)
+
+      return subfolderResult
 
   def formAdressString(name):
     result = adressArr[0]
@@ -27,24 +30,12 @@ def file_search(folder, fileName): # list, str
     result = result + '/' + name
     return result
 
-  iterateDir(folder)
+  return iterateDir(folder)
 
+# print 'test1, simple              : ' + str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt') )
+# print 'test3, in subfolder        : ' + str(file_search(['C:', 'backup.log', ['dir', 'ideas.txt']], 'ideas.txt') )
+print 'test3, empty folder before : ' + str(file_search(['C:', 'backup.log', ['dir'], 'ideas.txt'], 'ideas.txt') )
 
-
-
-  # for item in folder[1:]:
-  #   # print 'item: ' + str(item)
-  #   if isinstance(item, list):
-  #     # sublist iteration
-  #     a = 1
-  #   else:
-  #     if item == fileName:
-  #       result = result + fileName
-  #       return result
-
-  # return False
-
-print str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt') )
 # file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me')
 # file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py')
 
