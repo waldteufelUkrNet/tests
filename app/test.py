f7215eeae449d67ce62013a2894f1586931c1edc
waldteufelUkrNet <waldteufel@ukr.net>
2021-06-08 18:53:19 +0300

    q

--- app/test.py
+++ app/test.py
@@ -1,73 +1,70 @@
-def file_search(folder, fileName): # list, str
-  folderName = ''
-  result = ''
-  adressArr  = []
-  parentFoldersArr = []
+morse_code = {
+  "A" : ".-",
+  "B" : "-...",
+  "C" : "-.-.",
+  "D" : "-..",
+  "E" : ".",
+  "F" : "..-.",
+  "G" : "--.",
+  "H" : "....",
+  "I" : "..",
+  "J" : ".---",
+  "K" : "-.-",
+  "L" : ".-..",
+  "M" : "--",
+  "N" : "-.",
+  "O" : "---",
+  "P" : ".--.",
+  "Q" : "--.-",
+  "R" : ".-.",
+  "S" : "...",
+  "T" : "-",
+  "U" : "..-",
+  "V" : "...-",
+  "W" : ".--",
+  "X" : "-..-",
+  "Y" : "-.--",
+  "Z" : "--.."
+}
 
-  def iterateDir(arr, pos = 1):
-    arrLength = len( arr )
-    parentFoldersArr.append(arr)
+def encode_morze(text): # string
+  # return string
+  # . = ^
+  # - = ^^^
+  # after point or dash 1 _
+  # after letter 3 _
+  # after world 7 _
+  # after message 0 _
+  # only english letter lowercase/uppercase
 
-    if arrLength == 1: # folder is empty and only has a name ( arr['some name'] )
-
-      return False
-
-    else:              # folder is't empty, has a name and at less 1 item ( arr['some name', item 1, item 2, ... item n] )
-
-      folderName = arr[0]
-      adressArr.append(folderName)
-      subfolder = False
-
-      for item in arr[pos:]:
-        if isinstance(item,str): # file name
-          if item == fileName:
-            return formAdressString(item)
-        else:                    # new subfolder
-          subfolder = iterateDir(item)
-          if subfolder != False: # searched name is in subfolder
-            return subfolder
-
-      # subfolder iterarion is end. Must go up and iterate parent folder from subfolder next sibling
-      # parentFolder = parentFoldersArr[ len(parentFoldersArr) - 2 ]
-      # pos = parentFolder.index(arr)
-
-      # parentFolder = parentFolder[:-2]
-      # adressArr = adressArr[:-1]
-
-      # a = iterateDir(parentFolder, pos)
-      # if a != False:
-      #   return a
-
-    # return False
-
-  def formAdressString(name):
-    result = adressArr[0]
-    for item in adressArr[1:]:
-      result = result + '/' + item
-    result = result + '/' + name
-    return result
-
-  return iterateDir(folder)
-
-print 'test, second folder            : ' + str(file_search(['C:', ['eee', 'rrr', 'ttt'], ['yyy', 'ideas.txt']], 'ideas.txt') )
-
-# print 'test, empty folder            : ' + str(file_search(['C:'], 'ideas.txt') )
-# print 'test, simple                  : ' + str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt') )
-# print 'test, in subfolder            : ' + str(file_search(['C:', 'backup.log', ['dir', 'ideas.txt']], 'ideas.txt') )
-# print 'test, empty folder before     : ' + str(file_search(['C:', 'backup.log', ['dir'], 'ideas.txt'], 'ideas.txt') )
-# print 'test, not empty folder before : ' + str(file_search(['C:', 'backup.log', ['dir','q'], 'ideas.txt'], 'ideas.txt') )
-# print 'test, hard variant plus       : ' + str(file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py') )
-# print 'test, hard variant minus      : ' + str(file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me') )
+  def encode_word(word):
+    result_word = morse_code[ word[0] ]
 
 
+    return result_word
 
-# print '===== start ====='
-# print 'right result: C:/ideas.txt'
-# print 'my result   : ' + str(file_search(['C:', 'backup.log', 'ideas.txt'], 'ideas.txt'))
-# print '-----------------'
-# print 'right result: False'
-# print 'my result   : ' + str(file_search([ 'D:', ['recycle bin'], ['tmp', ['old'], ['new folder1', 'asd.txt', 'asd.bak', 'find.me.bak' ] ], 'hey.py'], 'find.me'))
-# print '-----------------'
-# print 'right result: /home/user2/desktop/new folder/hereiam.py'
-# print 'my result   : ' + str(file_search([ '/home', ['user1'], ['user2', ['my pictures'], ['desktop', 'not this', 'and not this', ['new folder', 'hereiam.py' ] ] ], 'work.ovpn', 'prometheus.7z', ['user3', ['temp'], ], 'hey.py'], 'hereiam.py'))
-# print '=====  end  ====='
+
+  string = text.upper()
+  wordsArr = string.split(' ')
+  print("wordsArr", len(wordsArr) )
+
+  result = encode_word( wordsArr[0] )
+
+  return result
+
+print '===== start ====='
+print 'right result: ^^^_^^^___^^^_^^^_^^^___^_^^^_^___^^^_^^^_^_^___^_______^^^_^_^^^_^___^^^_^^^_^^^___^^^_^_^___^0'
+print 'my result   : ' + str(encode_morze('Morze code'))
+print '-----------------'
+print 'right result: ^_^^^_^^^_^___^_^^^_^___^^^_^^^_^^^___^^^_^^^___^___^^^___^_^_^_^___^___^_^_^^^___^_^_^'
+print 'my result   : ' + str(encode_morze('Prometheus'))
+print '-----------------'
+print 'right result: ^_^_^___^^^_^^^_^^^___^_^_^'
+print 'my result   : ' + str(encode_morze('SOS'))
+print '-----------------'
+print 'right result: '
+print 'my result   : ' + str(encode_morze('1'))
+print '-----------------'
+print 'right result: '
+print 'my result   : ' + str(encode_morze(''))
+print '=====  end  ====='
