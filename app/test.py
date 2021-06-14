381f1d3f33d2a907a2e36cdc84a2a940bb841b5e
waldteufelUkrNet <waldteufel@ukr.net>
2021-06-14 21:35:35 +0300

    qqq

--- app/test.py
+++ app/test.py
@@ -1,71 +1,96 @@
-def find_most_frequent(text): # str: a-z ,.:;!?-
-  # return alphabetical list of words in lowercase > 2
-  # hand-made == hand, made
-  # page != pages
-  # page == Page
+def convert_n_to_m(x, n, m): # x - int/srt (upper/lower), n, m - int: 1 <= n, m <= 36
+  # return str: number x in system m
+  # return False: x != int/str or !!!
+  # osnova > 10: A-Z upper
+  # 1-system: zero-amount literation
 
-  l_text = text.lower()
-  wordsArr = []
-  currentWord = ''
+  alphaDigits = {'A':10,'B':11,'C':12,'D':13,'E':14,'F':15,'G':16,'I':17,'J':18,'K':19,'L':20,'M':21,'N':22,'O':23,'P':24,'Q':25,'R':26,'S':27,'T':28,'U':29,'V':30,'W':31,'X':32,'Y':33,'Z':34}
+  digitsAlpha = {10:'A',11:'B',12:'C',13:'D',14:'E',15:'F',16:'G',17:'I',18:'J',19:'K',20:'L',21:'M',22:'N',23:'O',24:'P',25:'Q',26:'R',27:'S',28:'T',29:'U',30:'V',31:'W',32:'X',33:'Y',34:'Z'}
 
-  # build array of all words
-  for letter in l_text:
-    if letter >= 'a' and letter <= 'z':
-      currentWord = currentWord + letter
-    else:
-      if len(currentWord) > 0:
-        wordsArr.append(currentWord)
-        currentWord = ''
+  # x validation
+  if isinstance(x, int) or isinstance(x, long):
+    x = str(x)
 
-  if len(currentWord) != 0:
-    wordsArr.append(currentWord)
+  if not isinstance(x, str):
+    return False
 
-  resultObj = {}
-  resultArr = []
-  copiedArr = []
-  for word in wordsArr:
-    copiedArr.append(word)
+  numberAsArray = []
+  number = 0L
+  for letter in x:
+    if (letter >= 'A' and letter <= 'Z') or (letter >= 'a' and letter <= 'z'):
+      letter = alphaDigits[letter.upper()]
+    if int(letter) >= n:
+      return False
+    numberAsArray.append( int(letter) )
+  numberAsArray.reverse()
 
-  for word in wordsArr:
-    count = 0
-    for word2 in copiedArr:
-      if str(word) == str(word2):
-        count = count + 1
+  iterator = 0
+  for item in numberAsArray:
+    number = number + numberAsArray[iterator]*(n**iterator)
+    iterator = iterator + 1
 
-    if count > 1 and not word in resultArr:
-      resultObj[word] = count
+  # 1. n-number -> 10-number
+  # try:
+  #   number = int(x, base=n)
+  # except:
+  #   return False
 
-  max_count = 0
-  for key in resultObj:
-    if resultObj[key] > max_count:
-      max_count = resultObj[key]
+  # 1.a
+  if m == 1:
+    return '0'*number
 
-  for key in resultObj:
-    if resultObj[key] == max_count:
-      resultArr.append(key)
+  # 2. 10-number -> m-number
+  string = ''
+  if number == 0:
+    return '0'
+  digits = []
+  while number:
+      digits.append(int(number % m))
+      number //= m
 
-  resultArr.sort()
-  return resultArr
+  for num in digits[::-1]:
+    if num > 9:
+      num = digitsAlpha[num]
+    string = string + str(num)
+  return string
 
 print '===== start ====='
-print "right result: ['hello']"
-print 'my result   : ' + str( find_most_frequent('Hello,Hello, my dear!') )
+print "right result: False"
+print 'my result   : ' + str( convert_n_to_m([123], 4, 3) )
 print '-----------------'
-print "right result: ['recursion', 'to', 'understand']"
-print 'my result   : ' + str( find_most_frequent('to understand recursion you need first to understand recursion...') )
+print "right result: 102"
+print 'my result   : ' + str( convert_n_to_m("0123", 5, 6) )
 print '-----------------'
-print "right result: ['mom']"
-print 'my result   : ' + str( find_most_frequent('Mom! Mom! Are you sleeping?!!!') )
+print "right result: False"
+print 'my result   : ' + str( convert_n_to_m("123", 3, 5) )
 print '-----------------'
-print "right result: ['mike']"
-print 'my result   : ' + str( find_most_frequent('Mike-Paul mike lol') )
+print "right result: 000000000000000000000000000"
+print 'my result   : ' + str( convert_n_to_m(123, 4, 1) )
 print '-----------------'
-print "right result: ['hello']"
-print 'my result   : ' + str( find_most_frequent("Load up on guns; bring your friends It's fun to lose and to pretend She's over-bored and self-assured Oh no, I know a dirty word Hello,hello,hello,how low Hello,hello,hello,howlow Hello,hello,hello,how low Hello,hello,hello") )
+print "right result: False"
+print 'my result   : ' + str( convert_n_to_m(-123.0, 11, 16) )
 print '-----------------'
-print "right result: ['i']"
-print 'my result   : ' + str( find_most_frequent("And I forget just why I taste; Oh yeah, I guess it makes me smile; I found it hard; it's hard to find; Oh well, whatever, never mind") )
+print "right result: 32E7"
+print 'my result   : ' + str( convert_n_to_m("A1Z", 36, 16) )
 print '-----------------'
-print "right result: ['pages', 'the']"
-print 'my result   : ' + str( find_most_frequent('This is the front page of the Simple English Wikipedia. Wikipedias are places where people work together to write encyclopedias in different languages. We use Simple English words and grammar here. The Simple English Wikipedia is for everyone! That includes children and adults who are learning English. There are 120,571 articles on the Simple English Wikipedia. All of the pages are free to use. They have all been published under both the Creative Commons Attribution/Share-Alike License 3.0 and GNU Free Documentation License. You can help here! You may change these pages and make new pages. Read help pages and other good pages to learn how to write pages here. If you need help, you may ask questions at Simple talk.') )
+print "right result: 0"
+print 'my result   : ' + str( convert_n_to_m(0, 10, 2) )
+print '-----------------'
+print "right result: 0"
+print 'my result   : ' + str( convert_n_to_m(000, 10, 2) )
+print '-----------------'
+print "right result: 0"
+print 'my result   : ' + str( convert_n_to_m('000', 10, 2) )
+print '-----------------'
+print "right result: 1100001001"
+print 'my result   : ' + str( convert_n_to_m(777L, 10, 2) )
+print '-----------------'
+print "right result: 2C09BC518E8048D23A"
+print 'my result   : ' + str( convert_n_to_m(123123123123123123123, 11, 16) )
+print '-----------------'
+print "right result: 123123123123123123123"
+print 'my result   : ' + str( convert_n_to_m(123123123123123123123, 10, 10) )
+print '-----------------'
+print "right result: HGPEYJ"
+print 'my result   : ' + str( convert_n_to_m('qweasd', 33, 36) )
 print '=====  end  ====='
